6c2ab07416472a18814f657bdd5605c669c4eac9
conti29 <xllikewine@126.com>
2018-07-13 22:39:45 +0800

    首页完成

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -2,14 +2,47 @@
 //关闭进度条
 
 $(document).ajaxStart(function(){
-    console.log( "ajax请求开始" );
+    
     NProgress.start();
-})
+});
+
+
 
 $(document).ajaxStop(function(){
-    console.log( "ajax请求结束" );
-setInterval(function(){
+setTimeout(function(){
     NProgress.done();
-},500)
+},500);
 
+});
+
+
+//二级菜单显示与隐藏
+$(".jibie").prev().on("click",function(){
+    $(this).next().stop().slideToggle();
+})
+
+$(".hidden_aslide").on("click",function(){
+    $(".lt_aside").toggleClass("active");
+    $(".view").toggleClass("active");
+})
+
+
+//退出功能
+//模态框的显示与隐藏
+$(".show_modal").on("click",function(){
+$('#logoutModal').modal('show');
+})
+
+//向后台发送请求退出
+$("#modal_out").on("click",function(){
+    $.ajax({
+        type:"get",
+        url:"/employee/employeeLogout",
+        success:function(info){
+           if(info.success){
+               location.href = "login.html";
+           }
+        }
+
+    })
 })
